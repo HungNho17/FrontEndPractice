a0ce80edf38a1340c9cd9527aeeac31ff38a8714
HungNho <nphung75@gmail.com>
2020-09-05 18:30:14 +0700

    Init setup for electron builder.

--- src/react.tsx
+++ src/react.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const Index = () => {
-  return <div>Hello React Hung!</div>;
-};
+class Index extends React.Component {
+  render() {
+    return <div>Hello React</div>;
+  }
+}
 
 ReactDOM.render(<Index />, document.getElementById('app'));
